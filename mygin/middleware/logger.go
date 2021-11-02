3c05806b814a59994817a364e25d19e2df382d4b
southli <961065551@qq.com>
2021-11-02 15:48:33 +0800

    内置一个logger中间件

---
+++ mygin/middleware/logger.go
@@ -0,0 +1,15 @@
+package middleware
+
+import (
+	"log"
+	"mygin"
+	"time"
+)
+
+func Logger() mygin.HandlerFunc {
+	return func(c *mygin.Context) {
+		t := time.Now()
+		c.Next()
+		log.Printf("[%d] %s in %v", c.StatusCode, c.Req.RequestURI, time.Since(t))
+	}
+}
