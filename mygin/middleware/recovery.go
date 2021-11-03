b3bd13bfb1e6234140deeeeb5e867a31f7b4dcfb
southli <961065551@qq.com>
2021-11-03 14:11:22 +0800

    新增了错误恢复的中间件

---
+++ mygin/middleware/recovery.go
@@ -0,0 +1,36 @@
+package middleware
+
+import (
+	"fmt"
+	"log"
+	"myWeb/mygin"
+	"net/http"
+	"runtime"
+	"strings"
+)
+
+func trace(message string) string {
+	var pcs [32]uintptr
+	n := runtime.Callers(3, pcs[:])
+	var str strings.Builder
+	str.WriteString(message + "\nTraceback:")
+	for _, pc := range pcs[:n] {
+		fn := runtime.FuncForPC(pc)
+		file, line := fn.FileLine(pc)
+		str.WriteString(fmt.Sprintf("\n\t%s:%d", file, line))
+	}
+	return str.String()
+}
+
+func Recovery() mygin.HandlerFunc {
+	return func(c *mygin.Context) {
+		defer func() {
+			if err := recover(); err != nil {
+				message := fmt.Sprintf("%s", err)
+				log.Printf("%s\n\n", trace(message))
+				c.Fail(http.StatusInternalServerError, "Internal Server Error")
+			}
+		}()
+		c.Next()
+	}
+}
